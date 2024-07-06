410a3b77c52b47ad9ab03ef3d256c1efa5b05126
Jonny <81505820+Tony-jdl@users.noreply.github.com>
2024-07-06 00:52:04 -0700

    creazione progetto spring boot

---
+++ src/main/java/com/tdconsulting/gestore_accessi/GestoreAccessiApplication.java
@@ -0,0 +1,13 @@
+package com.tdconsulting.gestore_accessi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreAccessiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoreAccessiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdconsulting/gestore_accessi/GestoreAccessiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdconsulting.gestore_accessi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreAccessiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
